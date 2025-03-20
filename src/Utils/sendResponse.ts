a4ebe768dbacb8883c3867e1744351f5cbcbc464
mominulislamsharon <ilsamikchannel328250@gmail.com>
2025-03-21 03:28:28 +0600

    added error handling

---
+++ src/Utils/sendResponse.ts
@@ -0,0 +1,19 @@
+import { Response } from 'express';
+
+type TSuccessResponse<T> = {
+  message: string;
+  status: boolean;
+  statusCode: number;
+  data?: T;
+};
+
+const sendResponse = <T>(res: Response, data: TSuccessResponse<T>) => {
+  // Send response to client
+  res.status(data.statusCode).json({
+    message: data.message,
+    status: true,
+    data: data.data,
+  });
+};
+
+export default sendResponse;
